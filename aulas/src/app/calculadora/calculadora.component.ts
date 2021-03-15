6ec1425bcdc4af8374b77c5b2a6749f56f1db78c
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2021-03-15 13:07:56 +0000

    basic calculator

--- aulas/src/app/calculadora/calculadora.component.ts
+++ aulas/src/app/calculadora/calculadora.component.ts
@@ -12,4 +12,47 @@ export class CalculadoraComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  num1: number = 0;
+  num2: number = 0;
+  operation: string = "+";
+  result: number = 0;
+
+
+
+  quickOperation(): number {
+
+    switch (this.operation) {
+      case "+":
+        if (this.num2 == 0) {
+          return this.result = this.num1;
+
+        } else if (this.num1 == 0) {
+          return this.result = this.num2;
+
+        } else if (this.num1 != 0 && this.num2 != 0) {
+          return this.result = +this.num1 + +this.num2;
+        }
+
+        break;
+
+      case "-":
+        return this.result = this.num1 - this.num2;
+
+      case "*":
+        return this.result = this.num1 * this.num2;
+
+      case "/":
+        return this.result = this.num1 / this.num2;
+
+        break;
+      default:
+        break;
+    }
+  }
+
+  clearFieldOnClick(event): any {
+    event.target.value = "";
+    this.result = 0;
+  }
+
 }
