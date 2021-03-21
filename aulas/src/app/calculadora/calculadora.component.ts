209779fc7878e8f29f2106afb12a1fa3ffec7f81
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2021-03-21 03:52:51 +0000

    Calculator fully done

--- aulas/src/app/calculadora/calculadora.component.ts
+++ aulas/src/app/calculadora/calculadora.component.ts
@@ -15,9 +15,7 @@ export class CalculadoraComponent implements OnInit {
   num1: number = 0;
   num2: number = 0;
   operation: string = "+";
-  result: number = 0;
-
-
+  result2: number = 0;
 
   quickOperation(): number {
 
@@ -54,5 +52,168 @@ export class CalculadoraComponent implements OnInit {
     event.target.value = "";
     this.result = 0;
   }
+
+  reapeatCheck: boolean = false;
+  numChecker: boolean = false;
+  checker: boolean = false;
+  operator: string = "";
+  n1: string = "";
+  n2: string = "";
+  result: any = 0;
+
+  doTheMath(): number | string {
+    this.numChecker = false;
+    this.reapeatCheck = false;
+    switch (this.operator) {
+      case "+":
+        if (this.n2 == "0") {
+          return this.result = this.n1;
+
+        } else if (this.n1 == "0") {
+          return this.result = this.n2;
+
+        } else if (this.n1 != "0" && this.n2 != "0") {
+          this.result = +this.n1 + +this.n2;
+          if (this.checker) {
+            this.numChecker = true;
+            this.n1 = this.result;
+          } else {
+            this.n1 = "";
+          }
+          this.n2 = "";
+          return this.result;
+        }
+        break;
+
+      case "-":
+        this.result = +this.n1 - +this.n2;
+        if (this.checker) {
+          this.n1 = this.result;
+        } else {
+          this.n1 = "";
+        }
+        this.n2 = "";
+        return this.result;
+
+      case "*":
+        this.result = +this.n1 * +this.n2;
+        if (this.checker) {
+          this.n1 = this.result;
+        } else {
+          this.n1 = "";
+        }
+        this.n2 = "";
+        return this.result;
+
+      case "/":
+        this.result = +this.n1 / +this.n2;
+        if (this.checker) {
+          this.n1 = this.result;
+        } else {
+          this.n1 = "";
+        }
+        this.n2 = "";
+        return this.result;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  setNumber(x): string {
+    if (!this.numChecker) {
+      if (!this.reapeatCheck) {
+        this.checker = false;
+        if (!this.checker) {
+          this.n1 = "";
+          this.reapeatCheck = true;
+        }
+      }
+      this.n1 += x;
+      this.result = this.n1;
+      return this.n1;
+
+    } else {
+      this.n2 += x;
+      this.result = this.n2;
+      return this.n2;
+    }
+  }
+
+  setOperator(x): string {
+    this.numChecker = true;
+    this.checker = true;
+    this.result = x;
+    return this.operator = x;
+  }
+
+  clear(): any {
+    this.result = 0;
+    this.n1 = "";
+    this.n2 = "";
+    this.operator = "";
+    this.numChecker = false;
+    this.checker = false;
+    this.reapeatCheck = false;
+  }
+
+  //#region numeros dos botoes + operadores
+  setPlus(): string {
+    return this.setOperator("+");
+  }
+
+  setMinus(): string {
+    return this.setOperator("-");
+  }
+
+  setSplit(): string {
+    return this.setOperator("/");
+  }
+
+  setMultiply(): string {
+    return this.setOperator("*");
+  }
+
+  set0(): number | string {
+    return this.setNumber(0);
+  }
+
+  set1(): number | string {
+    return this.setNumber(1);
+  }
+
+  set2(): number | string {
+    return this.setNumber(2);
+  }
+
+  set3(): number | string {
+    return this.setNumber(3);
+  }
+
+  set4(): number | string {
+    return this.setNumber(4);
+  }
+
+  set5(): number | string {
+    return this.setNumber(5);
+  }
+
+  set6(): number | string {
+    return this.setNumber(6);
+  }
+
+  set7(): number | string {
+    return this.setNumber(7);
+  }
+
+  set8(): number | string {
+    return this.setNumber(8);
+  }
+
+  set9(): number | string {
+    return this.setNumber(9);
+  }
+  //#endregion
 
 }
